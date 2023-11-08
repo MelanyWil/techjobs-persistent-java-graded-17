56b8f484997859a705e0a899173e13778bfad16b
Melany Willis <melexiw@gmail.com>
2023-11-08 10:29:44 -0600

    Uncommented tests and ran query

--- queries.sql
+++ queries.sql
@@ -1,7 +1,20 @@
---Part 1
+-- Part 1
+-- Id INTEGER
+-- Employer STRING
+-- Name STRING
+-- Skills STRING
 
---Part 2
+-- Part 2
+SELECT name
+FROM employer
+WHERE location = "St. Louis City";
 
---Part 3
+-- Part 3
+DROP TABLE job;
 
---Part 4
+-- Part 4
+SELECT *
+FROM skill
+INNER JOIN job_skills on skill.id = job_skills.skills_id
+WHERE job_skills.jobs_id IS NOT NULL
+ORDER BY name ASC;
